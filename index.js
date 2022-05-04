31de4e2781d5ba5a59f85c032c8324307e7b6efd
Valnei Santos <valnei.silva@unidompedro.com>
2022-05-03 21:47:31 -0300

    Adicionando regex

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+const texto = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)';
+
+function extraiLinks(text) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s]*.[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+
+    while ((temp = regex.exec(texto)) !== null) {
+        
+    }
+
+
+    const linksExtraidos = 
+    console.log(linksExtraidos);
+}
+
+extraiLinks(texto);
+
 const trataErro = (erro) => {
     throw new Error(chalk.red(erro.code, 'sem arquivo'));
 }
@@ -35,4 +53,4 @@ async function pegaArquivo (caminhoDoArquivo) {
 //     })
 // }
 
-pegaArquivo('./arquivos/texto1.md')
+// pegaArquivo('./arquivos/texto1.md')
